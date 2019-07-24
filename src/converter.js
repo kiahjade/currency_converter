2b5efd49c2ee6c57da31d4f18089898172916561
Kiah Hewitt <hewittkiah@gmail.com>
2019-07-24 19:32:45 +0100

    add source amount fee

--- src/converter.js
+++ src/converter.js
@@ -1,10 +1,11 @@
 const rate = 1.3066
+const sourceFee = 0.01
 const fixFee = 25 * rate
 
 module.exports = (amount) => {
   if (amount <= 1000) {
-    return amount * rate;
+    return (amount * rate) - ((amount * sourceFee) * rate);
   } else if (amount > 1000){
-    return (amount * rate) - (25 * rate);
+    return (amount * rate) - (fixFee);
   }
 }
--- test/converterTest.js
+++ test/converterTest.js
@@ -2,8 +2,8 @@ const converter = require('../src/converter')
 
 var assert = require('assert');
 describe('Converter', function() {
-  it('should return rate when 1 provided', function() {
-    assert.equal(converter(1), 1.3066);
+  it('should return rate with source fee deducted when 1 provided', function() {
+    assert.equal(converter(1), 1.293534);
   });
 
   it('should return take a £25 fee when 1000 provided', function() {
