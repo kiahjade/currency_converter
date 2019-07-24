7a29a0d84c660b23a331ebd8a8122f1c3021e237
Kiah Hewitt <hewittkiah@gmail.com>
2019-07-24 16:54:06 +0100

    should calculate 1000 correctly

--- test/converterTest.js
+++ test/converterTest.js
@@ -5,4 +5,8 @@ describe('Converter', function() {
   it('should return rate when 1 provided', function() {
     assert.equal(converter(1), 1.3066);
   });
+
+  it('should return 1306.6 when 1000 provided', function() {
+    assert.equal(converter(1000), 1306.6);
+  });
 });
