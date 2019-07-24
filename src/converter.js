62abfb911272ed9a47985b6f26463ca481912b40
Kiah Hewitt <hewittkiah@gmail.com>
2019-07-24 20:04:22 +0100

    returns 0 when negative amount passed

--- src/converter.js
+++ src/converter.js
@@ -3,7 +3,9 @@ const sourceFee = 0.01
 const fixFee = 25 * rate
 
 module.exports = (amount) => {
-  if (amount <= 1000) {
+  if (amount < 0) {
+    return 0
+  } else if (amount <= 1000) {
     return (amount * rate) - ((amount * sourceFee) * rate);
   } else if (amount > 1000){
     return (amount * rate) - (fixFee);
--- test/converterTest.js
+++ test/converterTest.js
@@ -9,4 +9,8 @@ describe('Converter', function() {
   it('should return take a £25 fee when 1000 provided', function() {
     assert.equal(converter(1001), 1275.2416);
   });
+
+  it('should return 0 when amount negative', function() {
+    assert.equal(converter(-100), 0);
+  });
 });
